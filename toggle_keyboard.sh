921b016026b8e27044df202b45080b2cdda1ba36
Jacek Plonowski <jplonowski@ucdavis.edu>
2019-10-29 21:27:58 -0700

    fixed toggle kb script

--- toggle_keyboard.sh
+++ toggle_keyboard.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
-# disables/enables default default keyboard using xinput
+# disables/enables specified keyboard using xinput
 DEVICE="AT Translated Set 2 keyboard"
+
+DEVICE_ID=$(xinput --list | grep "$DEVICE" | awk '{print $7}' | cut -d'=' -f2)
+
 STATUS=`xinput list-props "$DEVICE" | grep 'Device Enabled' | sed 's/.*\([0-9]\)$/\1/'`
+
 if [ "$STATUS" = "1" ]
 then
-    xinput disable 'AT Translated Set 2 Keyboard'
+    xinput disable "$DEVICE_ID"
     echo "Keyboard disabled"
 elif [ "$STATUS" = "0" ]
-the
-    xinput enable 'AT Translated Set 2 Keyboard'
+then
+    xinput enable "$DEVICE_ID"
     echo "Keyboard enabled"
 else
     echo "Error: bad argument"
